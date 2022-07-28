64758f9cba815a7c89f168aeb4f462add246c3c7
skhomuti <skhomuti@gmail.com>
2022-07-29 00:22:24 +0500

    Add lesson artifacts

--- test_simple.py
+++ test_simple.py
@@ -4,16 +4,24 @@ import time
 
 @pytest.fixture()
 def browser():
-    time.sleep(1)
+    """
+    Фикстура возвращает браузер
+    """
+    time.sleep(1.5)
 
 
+# @pytest.mark.skip(reason="Этот тест еще не завершен TASK-123")
 def test_first(browser):
     time.sleep(1)
 
 
+@pytest.mark.xfail(reason="Причина", raises=(AssertionError, ZeroDivisionError))
 def test_second():
-    time.sleep(1)
+    time.sleep(2)
 
 
 def test_third():
-    time.sleep(1)
+    import random
+    a = random.randint(0, 10)
+    assert a > 5
+    time.sleep(3)
